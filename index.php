102ed96f8d40d1e3b9f53358eac790f9086d1d47
Tyler Tirapelle <152163132+Shogunoftheitsoffice@users.noreply.github.com>
2025-10-20 16:08:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ if ($result && $result->num_rows > 0) {
     <div class="container">
         <div class="sidebar">
             <nav class="sidebar-nav">
-                <a href="#" class="nav-button"><img src="Assets/checkout.png" alt="" class="nav-icon"><span>Checkout</span></a>
                 <a href="#" id="search-btn" class="nav-button"><img src="Assets/search.png" alt="" class="nav-icon"><span>Search</span></a>
                 <a href="#" class="nav-button"><img src="Assets/add.png" alt="" class="nav-icon"><span>Add</span></a>
                 <a href="#" class="nav-button"><img src="Assets/edit.png" alt="" class="nav-icon"><span>Edit</span></a>
