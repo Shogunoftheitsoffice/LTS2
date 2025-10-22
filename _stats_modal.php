b74e23d77d5df31ee2ee8494b8ff5a6f3b046717
Tyler Tirapelle <152163132+Shogunoftheitsoffice@users.noreply.github.com>
2025-10-22 16:32:16 +0900

    Create _stats_modal.php

---
+++ _stats_modal.php
@@ -0,0 +1,9 @@
+<div id="stats-modal" class="modal-overlay">
+    <div class="modal-content">
+        <button class="modal-close">&times;</button>
+        <h2>Library Statistics</h2>
+        <div id="stats-content">
+            <p class="modal-message processing">Loading stats...</p>
+        </div>
+    </div>
+</div>
